9640e10ee418618e960ca1f45f01aca1ce6f90c6
Jayneeta Sinha <jsinha.info@gmail.com>
2021-03-07 15:48:12 +0530

    updating config express

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/achin-work'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/achin-work' }
-  );
-});
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/achin-work/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 console.log(`Running on port ${process.env.PORT || 8080}`)
